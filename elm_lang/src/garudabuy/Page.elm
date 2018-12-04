bc0850304c0e76f29d7bd1395fe65c6511a5b474
Sandesh Soni <hello@sandeshsoni.com>
2018-12-04 14:15:02 +0530

    add link to pages

--- elm_lang/src/garudabuy/Page.elm
+++ elm_lang/src/garudabuy/Page.elm
@@ -1,9 +1,10 @@
 module Garudabuy.Page exposing (..)
 
 import Garudabuy.Viewer as Viewer exposing (..)
+import Garudabuy.Route as Route exposing (..)
 
 import Browser exposing (Document)
-import Html exposing (Html)
+import Html exposing (Html, li, p, div, a, text)
 
 type Page
     = Home
@@ -13,12 +14,35 @@ type Page
 view : Maybe Viewer -> Page -> {title: String, content: Html msg} -> Document msg
 view maybeViewer page {title, content} =
     { title = title ++ " | Company name"
-    , body = [content]
+    , body = viewMenu page maybeViewer ++ [content]
     }
 
 
 -- header
+-- viewHeader: Page -> 
 
 -- side menu
+viewMenu : Page -> Maybe Viewer -> List (Html msg)
+viewMenu page maybeViewer =
+    let linkTo =
+            navbarLink page
+    in
+    case maybeViewer of
+        Just viewer ->
+            let
+                username =
+                    "abc"
+                    -- Viewer.username viewer
+            in
+                [ linkTo Route.Admin [ text "newPost " ]]
+
+        Nothing ->
+            [ linkTo Route.Admin [ text "Sign in" ]]
+
+navbarLink : Page -> Route -> List(Html msg) -> Html msg
+navbarLink page route linkContent =
+    li [][a[Route.href route] linkContent ]
+
+
 
 -- links to other pages
--- elm_lang/src/garudabuy/Route.elm
+++ elm_lang/src/garudabuy/Route.elm
@@ -3,7 +3,8 @@ module Garudabuy.Route exposing (..)
 import Url exposing (Url)
 import Browser.Navigation as Nav
 import Url.Parser as Parser exposing ((</>), Parser, oneOf, s, string)
-
+import Html.Attributes as Attr
+import Html exposing (Attribute)
 
 
 type Route
@@ -22,6 +23,24 @@ fromUrl : Url -> Maybe Route
 fromUrl url =
     { url | path = Maybe.withDefault "" url.fragment, fragment = Nothing }
         |> Parser.parse parser
+
+
+href : Route -> Attribute msg
+href targetRoute =
+    Attr.href (routeToString targetRoute)
+
+routeToString : Route -> String
+routeToString page =
+    let
+        pieces =
+            case page of
+                Home ->
+                    []
+                Admin ->
+                    ["admin"]
+
+    in
+        "#/" ++ String.join "/" pieces
 
 
 
