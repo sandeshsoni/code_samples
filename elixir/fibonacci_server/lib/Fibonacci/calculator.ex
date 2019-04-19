dc42bf325652ee3c9d6b635ebbab030dbfb9fc40
Sandesh Soni <hello@sandeshsoni.com>
2019-04-19 23:15:10 +0530

    tail call optimization

--- elixir/fibonacci_server/lib/Fibonacci/calculator.ex
+++ elixir/fibonacci_server/lib/Fibonacci/calculator.ex
@@ -1,9 +1,9 @@
 defmodule Fibonacci.Calculator do
 
 
-  def calculate(0), do: 0
-  def calculate(1), do: 1
-  def calculate(n), do: calculate(n-1) + calculate(n - 2)
+  # def calculate(0), do: 0
+  # def calculate(1), do: 1
+  # def calculate(n), do: calculate(n-1) + calculate(n - 2)
 
   # defp calculate(num1, num2) do
   #   # nextnum
@@ -12,7 +12,15 @@ defmodule Fibonacci.Calculator do
   # end
 
   # tail cal optimized
+  def calculate(0), do: 0
+  def calculate(1), do: 1
+  def calculate(n), do: calculate(n, 1, 0)
 
+  def calculate(0,_,result), do: result
+
+  def calculate(n, next, result) do
+    calculate(n-1, next + result, next)
+  end
 
 
 
