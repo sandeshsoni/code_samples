372d1385fc703a690caa91ae04f9dc5643f73eb2
Sandesh Soni <hello@sandeshsoni.com>
2019-04-20 18:27:06 +0530

    genserver add lookup method and table. wip

--- elixir/fibonacci_server/lib/fibonacci_server.ex
+++ elixir/fibonacci_server/lib/fibonacci_server.ex
@@ -1,4 +1,5 @@
 defmodule FibonacciServer do
+  use GenServer
 
   defdelegate calculate(num), to: Fibonacci.Calculator
 
@@ -8,6 +9,29 @@ defmodule FibonacciServer do
   defmodule Foo do
     defstruct bar_history: []
   end
+
+  def call_fib(foo) do
+    # add entry to struct / ets
+    calculate foo
+  end
+
+  def init(table_name) do
+    names = :ets.new(table_name, [:named_table, read_concurreny: true])
+    refs = %{}
+    {:ok, {names, refs}}
+  end
+
+  # server == tablename
+  def lookup(server, name) do
+    case :ets.lookup(server, name) do
+      [^name, pid] -> {:ok, pid}
+      [] -> :error
+    end
+  end
+
+  # def calculate(input) do
+  #   call_fib input
+  # end
 
   # TODO
   # historification
