df5b2d7a1dd005e01ed994b1e5cf6f470543d322
Sandesh Soni <hello@sandeshsoni.com>
2019-04-23 13:52:13 +0530

    add json lirary, use dummy data

--- elixir/fibonacci_server/lib/Fibonacci/router.ex
+++ elixir/fibonacci_server/lib/Fibonacci/router.ex
@@ -3,13 +3,20 @@ defmodule Fibonacci.Router do
 
   plug :match
   plug :dispatch
+  plug Plug.Parsers, parsers: [:json],
+    pass: ["text/*"],
+    json_decoder: Jason
 
   get("/hello") do
     send_resp(conn, 200, "world")
   end
 
-  get("/fibonacci") do
-    send_resp(conn, 200, "world")
+  get("/fib") do
+    body = Jason.encode!(%{ a: "b" })
+
+    conn
+    |> put_resp_content_type("text/json")
+    |> send_resp(200,body)
   end
 
 end
